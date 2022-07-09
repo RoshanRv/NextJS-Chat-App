108716ffdfdfb433ff3edbc6067ccb02f2aa7900
Roshan <nkroshankumar@gmail.com>
2022-07-09 16:46:01 +0530

    firebase config added

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth }from 'firebase/auth'
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +20,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth()
+const db = getFirestore()
 
-export {auth}
+export {auth,db}
